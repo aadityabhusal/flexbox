4598d0bbbabcf11c64b98c2a7a83809011c32923
aadityabhusal <bhusal.001aditya@gmail.com>
2019-02-18 08:21:57 +0545

    All files uploaded

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$("#right-slide").click(function(){
+		var left = $("#slide-list").offset().left;
+		if(left>="-2159"){
+			$("#slide-list").css('left',(left-270))
+		}
+	});
+
+	$("#left-slide").click(function(){
+		var right = $("#slide-list").offset().left;
+		if(right<="-249"){
+			$("#slide-list").css('left',(right+270));
+		}
+	});	
+});
